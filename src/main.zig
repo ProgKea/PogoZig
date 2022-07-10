92fab27bc2f5afed5ef33671e21d3b2757ed3fe5
ProgKea <hoschy01@googlemail.com>
2022-07-10 20:42:03 +0200

    rendered player, refactoring

--- src/main.zig
+++ src/main.zig
@@ -5,10 +5,16 @@ const c = @cImport({
 
 const FPS = 60;
 const DELTA_TIME_SEC: f32 = 1.0/@intToFloat(f32, FPS);
+
 const PLAYER_WIDTH = 100;
 const PLAYER_HEIGHT = 200;
 const PLAYER_INITIAL_X = 100;
 const PLAYER_INITIAL_Y = 100;
+const PLAYER_GRAVITY = 0.4;
+const PLAYER_VEL_Y_LIMIT = 25;
+
+const WINDOW_HEIGHT = 1000;
+const WINDOW_WIDTH = 1200;
 
 const directions = enum { left, right };
 
@@ -17,46 +23,49 @@ const Vec2 = struct {
     y: i32,
 };
 
-const Player = struct {
+const Platform = struct {
     rect: c.SDL_Rect,
-    angle: f16,
-
-    fn update(self: *Player) void {
-        _ = self;
-    }
+    color: c.SDL_Color,
 };
 
-fn NewPlayer() Player {
-    var player = Player{
-        .rect = c.SDL_Rect{ .x = PLAYER_INITIAL_X, .y = PLAYER_INITIAL_Y, .w = PLAYER_WIDTH, .h = PLAYER_HEIGHT },
-        .angle = 0.0,
-    };
+const Player = struct {
+    rect: c.SDL_Rect = c.SDL_Rect{ .x = PLAYER_INITIAL_X, .y = PLAYER_INITIAL_Y, .w = PLAYER_WIDTH, .h = PLAYER_HEIGHT },
+    vel_y: f32 = 0.0,
+    angle: f16 = 0.0,
 
-    return player;
-}
-
-fn RenderPlayer(renderer: *c.SDL_Renderer, player: Player) void {
-    _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    _ = c.SDL_RenderFillRect(renderer, &player.rect);
-}
-
-// TODO: implement PlayerMove 
-fn PlayerMove(direction: directions, player: *Player) void {
-    switch (direction) {
-        directions.left => {
-            std.debug.print("{d}\n", .{player.*.angle});
-        },
-        directions.right => {
-            std.debug.print("{d}\n", .{player.*.angle});
-        },
+    fn render(self: *Player, renderer: *c.SDL_Renderer) void {
+        _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        _ = c.SDL_RenderFillRect(renderer, &self.rect);
     }
-}
+
+    // TODO: implement movement function 
+    fn move(self: *Player, direction: directions) void {
+        switch (direction) {
+            directions.left => {
+                std.debug.print("{d}\n", .{self.*.angle});
+            },
+            directions.right => {
+                std.debug.print("{d}\n", .{self.*.angle});
+            },
+        }
+    }
+
+    fn applyGravity(self: *Player) void {
+        if (self.*.vel_y < PLAYER_VEL_Y_LIMIT) self.*.vel_y += PLAYER_GRAVITY
+        else self.*.vel_y = PLAYER_VEL_Y_LIMIT;
+        self.*.rect.y += @floatToInt(i32, self.*.vel_y);
+    }
+
+    fn update(self: *Player) void {
+        self.applyGravity();
+    }
+};
 
 pub fn main() anyerror!void {
     _ = c.SDL_Init(c.SDL_INIT_VIDEO);
     defer c.SDL_Quit();
 
-    const window = c.SDL_CreateWindow("PogoZig", 0, 0, 500, 500, 0) orelse {
+    const window = c.SDL_CreateWindow("PogoZig", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0) orelse {
         c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
@@ -68,28 +77,26 @@ pub fn main() anyerror!void {
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    var player = NewPlayer();
-
-    mainloop: while (true) {
+    var player = Player {};
+    gameloop: while (true) {
         var sdl_event: c.SDL_Event = undefined;
         const keyboard = c.SDL_GetKeyboardState(null);
         while (c.SDL_PollEvent(&sdl_event) != 0) {
             switch (sdl_event.type) {
-                c.SDL_QUIT => break :mainloop,
+                c.SDL_QUIT => break :gameloop,
                 else => {},
             }
         }
 
-        if (keyboard[c.SDL_SCANCODE_Q] != 0) break :mainloop;
-
-        if (keyboard[c.SDL_SCANCODE_A] != 0) PlayerMove(directions.left, &player);
-        if (keyboard[c.SDL_SCANCODE_D] != 0) PlayerMove(directions.right, &player);
+        if (keyboard[c.SDL_SCANCODE_Q] != 0) break :gameloop;
+        if (keyboard[c.SDL_SCANCODE_A] != 0) player.move(directions.left);
+        if (keyboard[c.SDL_SCANCODE_D] != 0) player.move(directions.right);
 
         player.update();
 
         _ = c.SDL_SetRenderDrawColor(renderer, 28, 28, 28, 255);
         _ = c.SDL_RenderClear(renderer);
-        _ = RenderPlayer(renderer, player);
+        player.render(renderer);
         c.SDL_RenderPresent(renderer);
 
         c.SDL_Delay(1000/FPS);
